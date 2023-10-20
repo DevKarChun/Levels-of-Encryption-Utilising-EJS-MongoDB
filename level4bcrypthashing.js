21f73a46d9aad3636f09ad7501dce15aa37a886e
Kar Chun <dev.karchun@gmail.com>
2023-10-20 18:45:44 +0100

    commit v4

--- level4bcrypthashing.js
+++ level4bcrypthashing.js
@@ -1,14 +1,16 @@
-//hashing
+//Bcrypt hashing
 
 import bodyParser from "body-parser";
 import express from "express";
 import ejs from "ejs";
-import env from "dotenv"
+import env from "dotenv";
 import mongoose from "mongoose";
-import md5 from "md5"
+import bcrypt from "bcrypt";
 
 const app = express()
 env.config()
+
+const saltRounds = 10;
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -39,35 +41,33 @@ const userSchema = new mongoose.Schema({
 
 const User = new mongoose.model("User", userSchema)
 
-app.post("/register", function(req, res){
-    const newUser = new User({
+app.post('/register', (req, res) => {
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+      const newUser = new User({
         email: req.body.username,
-        password: md5(req.body.password)
-    });
-
-    newUser.save().then(() => {
-        res.render("secrets");
-    }).catch((err) => {
-        console.log(err);
+        password: hash,
+      })
+      newUser
+        .save()
+        .then(() => {
+          console.log('Successfully created a new user!')
+          res.render('secrets')
+        })
+        .catch((err) => {
+          console.error(err.message)
+        })
     })
-})
+  })
 
 app.post("/login", function (req, res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username}).then((userFound) =>{
-        if(userFound){
-            if(userFound.password === password){
+        bcrypt.compare(password, userFound.password, function(err, result){
+            if(result === true){
                 res.render("secrets")
-            }else{
-                console.log("login error")
             }
-        }else{
-            res.render("/login")
-            console.log("login error")
-        }
-    }).catch((err)=>{
-        console.log(err)
+        })
     });
 })
