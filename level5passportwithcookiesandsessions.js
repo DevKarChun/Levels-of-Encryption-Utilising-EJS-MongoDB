facb3ce1db194e42e37149f7e191a729088f794c
Kar Chun <dev.karchun@gmail.com>
2023-10-23 17:36:17 +0100

    commit v5

---
+++ level5passportwithcookiesandsessions.js
@@ -0,0 +1,56 @@
+//Bcrypt hashing
+
+import bodyParser from "body-parser";
+import express from "express";
+import ejs from "ejs";
+import env from "dotenv";
+import mongoose from "mongoose";
+import session from "express-session"
+import passport from "passport"
+import passportLocalMongoose from "passport-local-mongoose"
+
+const app = express()
+env.config()
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+    secret: process.env.secretForDatabase,
+    resave: false,
+    saveUninitialized: false;
+}));
+
+app.get("/", function(req, res){
+    res.render("home")
+})
+
+app.get("/login", function(req, res){
+    res.render("login")
+})
+
+app.get("/register", function(req, res){
+    res.render("register")
+})
+
+app.listen(process.env.port, function(){
+    console.log("Server started on port 3000")
+})
+
+mongoose.connect(process.env.connectMongoose, {useNewUrlParser: true})
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+})
+
+const User = new mongoose.model("User", userSchema)
+
+app.post('/register', (req, res) => {
+    
+  })
+
+app.post("/login", function (req, res){
+  
+})
