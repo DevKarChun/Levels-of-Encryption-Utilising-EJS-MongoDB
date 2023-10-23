fcc0abb7bde4f3357998234d512ae3cf43b3e828
Kar Chun <dev.karchun@gmail.com>
2023-10-23 17:43:10 +0100

    commit v5

--- level5passportwithcookiesandsessions.js
+++ level5passportwithcookiesandsessions.js
@@ -94,3 +94,15 @@ app.post("/login", function(req, res){
 
 });
 
+app.post("/register", function(req, res){
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }
+    });
+});
